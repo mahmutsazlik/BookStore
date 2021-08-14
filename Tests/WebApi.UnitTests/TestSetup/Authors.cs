36a41b0459a268c1ef0a7c1fea2fa3650d317f8c
mahmutsazlik <mahmutsazlik@gmail.com>
2021-08-15 02:00:30 +0300

    CommonTestFixture oluşturuldu. Test için InMemoryDb oluşturuldu. CreateBookCommandTests içerisinde ilk test oluşturuldu.

---
+++ Tests/WebApi.UnitTests/TestSetup/Authors.cs
@@ -0,0 +1,18 @@
+using System;
+using WebApi.DbOperations;
+using WebApi.Models;
+
+namespace TestSetup
+{
+    public static class Authors
+    {
+        public static void AddAuthors(this BookStoreDbContext context)
+        {
+            context.Authors.AddRange(
+                    new Author() { FirstName = "AuthorFN1", LastName = "AuthorLN1", Birthday = new DateTime(1990, 10, 05) },
+                    new Author() { FirstName = "AuthorFN2", LastName = "AuthorLN2", Birthday = new DateTime(2000, 06, 18) },
+                    new Author() { FirstName = "AuthorFN3", LastName = "AuthorLN3", Birthday = new DateTime(1985, 04, 23) }
+                    );
+        }
+    }
+}
